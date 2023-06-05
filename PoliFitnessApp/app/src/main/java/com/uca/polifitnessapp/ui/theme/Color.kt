94b8051e0406cd382cc1cce6f41c6d7c77bd257d
FFigueroa17 <00019921@uca.edu.sv>
2023-06-05 15:04:01 -0600

    :construction: Add: Theme colors

--- PoliFitnessApp/app/src/main/java/com/uca/polifitnessapp/ui/theme/Color.kt
+++ PoliFitnessApp/app/src/main/java/com/uca/polifitnessapp/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.uca.polifitnessapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val PrimaryColor = Color(0xFF034189)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
