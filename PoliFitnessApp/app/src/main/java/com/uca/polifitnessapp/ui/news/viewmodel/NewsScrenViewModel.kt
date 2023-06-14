5786f40dfe01e77c6111e2a2b505924fb03510e4
Cheesypops <00030821@uca.edu.sv>
2023-06-14 09:45:49 -0600

    Add: newsRepository to newsViewModel

--- PoliFitnessApp/app/src/main/java/com/uca/polifitnessapp/ui/news/viewmodel/NewsScrenViewModel.kt
+++ PoliFitnessApp/app/src/main/java/com/uca/polifitnessapp/ui/news/viewmodel/NewsScrenViewModel.kt
@@ -1,8 +1,27 @@
 package com.uca.polifitnessapp.ui.news.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.paging.ExperimentalPagingApi
+import com.uca.polifitnessapp.PoliFitnessApplication
+import com.uca.polifitnessapp.repositories.NoticeRepository
+import com.uca.polifitnessapp.ui.news.data.NewsViewModel
 
 class NewsScrenViewModel(
-    
+    private val repository: NoticeRepository
 ) : ViewModel() {
+    @OptIn(ExperimentalPagingApi::class)
+    fun getNews(query: String = "%") = repository.getNewsPage(2, query)// cambiar aqui el parametro q recibe, pa q no sea quemado
+
+    companion object {
+        val Factory = viewModelFactory {
+            initializer {
+                val app =
+                    this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PoliFitnessApplication
+                NewsViewModel(app.noticeRepository)
+            }
+        }
+    }
 }
