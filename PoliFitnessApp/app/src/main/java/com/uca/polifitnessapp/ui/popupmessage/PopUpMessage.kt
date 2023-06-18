cb0c66492f9db78e732fc7d50bfdc1dee09deda6
pamelagomz <00170221.uca.edu.sv>
2023-06-18 13:48:47 -0600

    :construction: BottomSheet

--- PoliFitnessApp/app/src/main/java/com/uca/polifitnessapp/ui/popupmessage/PopUpMessage.kt
+++ PoliFitnessApp/app/src/main/java/com/uca/polifitnessapp/ui/popupmessage/PopUpMessage.kt
@@ -1,28 +1,47 @@
 package com.uca.polifitnessapp.ui.popupmessage
 
 
+import android.annotation.SuppressLint
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomSheetScaffoldState
+import androidx.compose.material.BottomSheetState
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -31,54 +50,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.uca.polifitnessapp.R
-
-
-
-
-
-
-@Preview(showBackground = true)
-@Composable
-fun PopUpCardScreen(){
-
-    // Error Card
-    PopupMessageComposable(
-        painterResource = painterResource(id = R.drawable.errorimage),
-        title = stringResource(R.string.title_error_card),
-        titleColor = MaterialTheme.colorScheme.error,
-        description = stringResource(R.string.description_error_card),
-        cardColors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.errorContainer),
-        buttonColor = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error),
-        buttonText =  stringResource(R.string.button_text_error_card)
-    )
-
-    //Profile updated successfully
-
-    PopupMessageComposable(
-        painterResource = painterResource(id = R.drawable.updatedimagecard),
-        title = stringResource(R.string.title_updated_profile_card),
-        titleColor = MaterialTheme.colorScheme.primary,
-        description = stringResource(R.string.description_updated_card),
-        cardColors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
-        buttonColor = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
-        buttonText =  "Listo"
-    )
-
-    //Contact message sent successfully
-
-    PopupMessageComposable(
-        painterResource = painterResource(id = R.drawable.contactcardimage),
-        title = stringResource(R.string.title_contact_card),
-        titleColor = MaterialTheme.colorScheme.primary,
-        description = stringResource(R.string.contact_card_description),
-        cardColors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
-        buttonColor = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
-        buttonText =  "Listo"
-    )
-
-
-
-}
+import com.uca.polifitnessapp.ui.signup.approachscreen.ui.ConfirmGoalButton
+import com.uca.polifitnessapp.ui.signup.approachscreen.ui.MainFunction
+import com.uca.polifitnessapp.ui.signup.approachscreen.ui.SignUpGoalText
+import kotlinx.coroutines.launch
 
 
 //Pop-Up cards composable function with parameters to change the content of the card
@@ -167,5 +142,120 @@ fun PopupMessageComposable(
 
     }
 }
+
+@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
+@Composable
+fun viewScreen(){
+
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState() // Crear el BottomSheetScaffoldState
+
+    BottomSheetScaffold(
+        scaffoldState = bottomSheetScaffoldState,
+        sheetContent = { SheetContent() } // Contenido del BottomSheet
+
+    ){
+
+    }
+
+
+
+}
+
+
+// Content of the BottomSheet
+@Composable
+fun SheetContent(){
+    PopupMessageComposable(
+        painterResource = painterResource(id = R.drawable.errorimage),
+        title = stringResource(R.string.title_error_card),
+        titleColor = MaterialTheme.colorScheme.error,
+        description = stringResource(R.string.description_error_card),
+        cardColors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.errorContainer),
+        buttonColor = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error),
+        buttonText =  stringResource(R.string.button_text_error_card)
+    )
+
+}
+
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PopUpCardScreen(){
+
+    // Error Card
+    PopupMessageComposable(
+        painterResource = painterResource(id = R.drawable.errorimage),
+        title = stringResource(R.string.title_error_card),
+        titleColor = MaterialTheme.colorScheme.error,
+        description = stringResource(R.string.description_error_card),
+        cardColors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.errorContainer),
+        buttonColor = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error),
+        buttonText =  stringResource(R.string.button_text_error_card)
+    )
+
+    //Profile updated successfully
+
+    PopupMessageComposable(
+        painterResource = painterResource(id = R.drawable.updatedimagecard),
+        title = stringResource(R.string.title_updated_profile_card),
+        titleColor = MaterialTheme.colorScheme.primary,
+        description = stringResource(R.string.description_updated_card),
+        cardColors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
+        buttonColor = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+        buttonText =  "Listo"
+    )
+
+    //Contact message sent successfully
+
+    PopupMessageComposable(
+        painterResource = painterResource(id = R.drawable.contactcardimage),
+        title = stringResource(R.string.title_contact_card),
+        titleColor = MaterialTheme.colorScheme.primary,
+        description = stringResource(R.string.contact_card_description),
+        cardColors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
+        buttonColor = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+        buttonText =  "Listo"
+    )
+
+
+
+}
+
+
+
+
+
+// Content of the BottomSheetScaffold
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
+@Composable
+fun Content(
+
+)
+{
+    val coroutineScope = rememberCoroutineScope()
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    bottomSheetScaffoldState.bottomSheetState.expand()
+                }
+            }
+        ) {
+            Text(text = "Abrir BottomSheet")
+        }
+    }
+}
+
 
 
