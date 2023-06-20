1e566be132902ec57e58c556809cb4ba79da868e
RicardoLopez <00107521@uca.edu.sv>
2023-06-20 10:46:04 -0600

    Rewrite: Terms and Conditions

--- PoliFitnessApp/app/src/main/java/com/uca/polifitnessapp/ui/politicscreen/ui/PoliticsScreen.kt
+++ PoliFitnessApp/app/src/main/java/com/uca/polifitnessapp/ui/politicscreen/ui/PoliticsScreen.kt
@@ -15,8 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,17 +23,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.uca.polifitnessapp.R
 import com.uca.polifitnessapp.ui.theme.md_theme_light_outline
-import com.uca.polifitnessapp.ui.theme.md_theme_light_scrim
-
 
 @Composable
 fun privacyPoliticsScreen(
